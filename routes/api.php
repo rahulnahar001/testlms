42cc0a58ed76fa95aadbb82badd1d88f52ca5ac9
Rahul Nahar <rnahar034@gmail.com>
2024-08-26 08:44:28 +0100

    delete cus

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\CustomerController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 
-Route::prefix('v1')->group(function () {
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
+});
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::apiResource('/customers', CustomerController::class);
 });
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class)->middleware('guest');
+    Route::post('/logout', LogoutController::class)->middleware('auth');
+    Route::post('/register', RegisterController::class)->middleware('auth');
 });
